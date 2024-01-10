05d5a0179387e8180479e17f2ccb6a3dfa602f79
erennner <155827482+erennner@users.noreply.github.com>
2024-01-10 19:09:45 +0300

    1
    1

---
+++ sibervatan.py
@@ -0,0 +1,17 @@
+sayı=-7
+print("mutlak değer",abs(sayı) )
+
+us=pow(2,3)
+print("sayı ussu",us)
+
+from math import sqrt
+sayi=25
+karekök=sqrt(sayi)
+print("sayının karekökü",karekök)
+
+from math import ceil , floor 
+ondalik=10.25
+asagi=floor(ondalik)
+yukarı=ceil(ondalik)
+print("yukarı",yukarı)
+print("asagğı",asagi)
